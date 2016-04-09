c318098135306541f3977b41c8e96c2f887c4c1b
Christian Stettler <cstettler@imap.ch>
2016-04-09 17:37:30 +0200

    added minimal sample application based on spring boot and embedded tomcat

---
+++ cebolla-sample/cebolla-sample-application/src/main/java/com/github/cstettler/cebolla/sample/application/CebollaSampleApplication.java
@@ -0,0 +1,11 @@
+package com.github.cstettler.cebolla.sample.application;
+
+import org.springframework.boot.SpringApplication;
+
+public class CebollaSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CebollaSampleApplicationConfiguration.class, args);
+  }
+
+}
---
+++ cebolla-sample/cebolla-sample-application/src/main/java/com/github/cstettler/cebolla/sample/application/CebollaSampleApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.cstettler.cebolla.sample.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebollaSampleApplicationConfiguration {
+
+}
