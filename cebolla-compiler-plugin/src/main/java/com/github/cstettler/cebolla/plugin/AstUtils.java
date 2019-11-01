bdf6212fdf93a9c5a52be3f1f21d37354c1e5d15
cstettler <cstettler@imap.ch>
2019-11-01 14:48:42 +0100

    refactored ast utility methods into utility class (for reuse in other stereotype handlers)

---
+++ cebolla-compiler-plugin/src/main/java/com/github/cstettler/cebolla/plugin/AstUtils.java
@@ -0,0 +1,259 @@
+package com.github.cstettler.cebolla.plugin;
+
+import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.code.TypeTag;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.Name;
+import com.sun.tools.javac.util.Names;
+
+import java.util.stream.Stream;
+
+import static com.sun.tools.javac.code.Flags.FINAL;
+import static com.sun.tools.javac.code.TypeTag.BOT;
+import static com.sun.tools.javac.tree.JCTree.JCArrayTypeTree;
+import static com.sun.tools.javac.tree.JCTree.JCBinary;
+import static com.sun.tools.javac.tree.JCTree.JCBlock;
+import static com.sun.tools.javac.tree.JCTree.JCClassDecl;
+import static com.sun.tools.javac.tree.JCTree.JCExpression;
+import static com.sun.tools.javac.tree.JCTree.JCFieldAccess;
+import static com.sun.tools.javac.tree.JCTree.JCIdent;
+import static com.sun.tools.javac.tree.JCTree.JCIf;
+import static com.sun.tools.javac.tree.JCTree.JCLiteral;
+import static com.sun.tools.javac.tree.JCTree.JCMethodDecl;
+import static com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
+import static com.sun.tools.javac.tree.JCTree.JCPrimitiveTypeTree;
+import static com.sun.tools.javac.tree.JCTree.JCReturn;
+import static com.sun.tools.javac.tree.JCTree.JCStatement;
+import static com.sun.tools.javac.tree.JCTree.JCUnary;
+import static com.sun.tools.javac.tree.JCTree.JCVariableDecl;
+import static com.sun.tools.javac.tree.JCTree.Tag.EQ;
+import static com.sun.tools.javac.tree.JCTree.Tag.MUL;
+import static com.sun.tools.javac.tree.JCTree.Tag.NE;
+import static com.sun.tools.javac.tree.JCTree.Tag.NOT;
+import static com.sun.tools.javac.tree.JCTree.Tag.OR;
+import static com.sun.tools.javac.tree.JCTree.Tag.PLUS;
+import static com.sun.tools.javac.util.List.from;
+import static com.sun.tools.javac.util.List.nil;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+class AstUtils {
+
+    private static final ThreadLocal<Context> CONTEXT_THREAD_LOCAL = new ThreadLocal<>();
+
+    private AstUtils() {
+    }
+
+    static void with(Context context, Runnable runnable) {
+        try {
+            CONTEXT_THREAD_LOCAL.set(context);
+
+            runnable.run();
+        } finally {
+            CONTEXT_THREAD_LOCAL.remove();
+        }
+    }
+
+    static void addMethod(JCClassDecl classDecl, JCMethodDecl equalsMethodDecl) {
+        classDecl.defs = classDecl.defs.append(equalsMethodDecl);
+    }
+
+    static List<JCExpression> arguments(Stream<JCExpression> stream) {
+        return from(stream.collect(toList()));
+    }
+
+    static List<JCExpression> arguments(JCExpression... expressions) {
+        return from(asList(expressions));
+    }
+
+    static JCMethodDecl method(int modifier, TypeTag returnType, String name, List<JCVariableDecl> parameters, JCBlock equalsBody) {
+        return factory().MethodDef(
+                factory().Modifiers(modifier),
+                names().fromString(name),
+                factory().TypeIdent(returnType),
+                nil(),
+                parameters,
+                nil(),
+                equalsBody,
+                null
+        );
+    }
+
+    static Stream<JCVariableDecl> fieldsOf(JCClassDecl classDeclaration) {
+        return classDeclaration.getMembers().stream()
+                .filter((member) -> member instanceof JCVariableDecl)
+                .map((member) -> (JCVariableDecl) member);
+    }
+
+    static JCVariableDecl parameter(JCClassDecl classDeclaration, String parameterName, Type parameterType) {
+        return factory().at(classDeclaration.pos).Param(names().fromString(parameterName), parameterType, null);
+    }
+
+    static JCBlock block(List<JCStatement> statements) {
+        return factory().Block(0, statements);
+    }
+
+    static JCUnary not(JCExpression expression) {
+        return factory().Unary(NOT, expression);
+    }
+
+    static boolean isArray(JCVariableDecl fieldDeclaration) {
+        return fieldDeclaration.vartype instanceof JCArrayTypeTree;
+    }
+
+    static JCExpression identifier(Name name) {
+        return factory().Ident(name);
+    }
+
+    static boolean isPrimitive(JCVariableDecl fieldDeclaration) {
+        return fieldDeclaration.vartype instanceof JCPrimitiveTypeTree;
+    }
+
+    static boolean isPrimitiveOfType(JCVariableDecl fieldDeclaration, TypeTag typeTag) {
+        return isPrimitive(fieldDeclaration)
+                && fieldDeclaration.vartype instanceof JCPrimitiveTypeTree
+                && ((JCPrimitiveTypeTree) fieldDeclaration.vartype).typetag == typeTag;
+    }
+
+    static JCLiteral falze() {
+        return factory().Literal(FALSE);
+    }
+
+    static JCLiteral nuull() {
+        return factory().Literal(BOT, null);
+    }
+
+    static JCBinary or(JCExpression left, JCExpression right) {
+        return factory().Binary(OR, left, right);
+    }
+
+    static JCBinary isNotEqual(JCExpression left, JCExpression right) {
+        return factory().Binary(NE, left, right);
+    }
+
+    static JCBinary add(JCExpression left, JCExpression right) {
+        return factory().Binary(PLUS, left, right);
+    }
+
+    static JCBinary multiply(JCExpression left, JCExpression right) {
+        return factory().Binary(MUL, left, right);
+    }
+
+    static JCStatement reAssignVariable(JCIdent variable, JCBinary expression) {
+        return factory().Exec(factory().Assign(variable, expression));
+    }
+
+    static JCMethodInvocation callTo(JCExpression method, JCExpression... arguments) {
+        return callTo(method, from(arguments));
+    }
+
+    static JCMethodInvocation callTo(JCExpression method, List<JCExpression> arguments) {
+        return factory().Apply(nil(), method, arguments);
+    }
+
+    static JCFieldAccess fieldOrMethod(String fieldOrMethod) {
+        return factory().Select(thiz(), names().fromString(fieldOrMethod));
+    }
+
+    static JCFieldAccess fieldOrMethod(String target, String fieldOrMethod) {
+        return fieldOrMethod(target, names().fromString(fieldOrMethod));
+    }
+
+    static JCFieldAccess fieldOrMethod(String target, Name fieldOrMethod) {
+        if (target.contains(".")) {
+            String[] classNameParts = target.split("\\.");
+            JCExpression classIdentifier = identifier(classNameParts[0]);
+
+            for (int i = 1; i < classNameParts.length; i++) {
+                classIdentifier = factory().Select(classIdentifier, names().fromString(classNameParts[i]));
+            }
+
+            return factory().Select(
+                    classIdentifier,
+                    fieldOrMethod
+            );
+        } else {
+            return factory().Select(identifier(target), fieldOrMethod);
+        }
+    }
+
+    static JCVariableDecl cast(boolean makeFinal, String targetVariableName, String sourceVariableName, JCClassDecl targetType) {
+        return variable(
+                makeFinal,
+                identifier(targetType.name),
+                targetVariableName,
+                factory().TypeCast(identifier(targetType.name), identifier(sourceVariableName))
+        );
+    }
+
+    static JCVariableDecl variable(boolean makeFinal, Type type, String name, JCExpression initialization) {
+        return variable(makeFinal, factory().Type(type), name, initialization);
+    }
+
+    static JCVariableDecl variable(boolean makeFinal, JCExpression type, String name, JCExpression initialization) {
+        return factory().VarDef(
+                factory().Modifiers(makeFinal ? FINAL : 0),
+                names().fromString(name),
+                type,
+                initialization
+        );
+    }
+
+    static JCIf iif(JCExpression condition, JCStatement body) {
+        return factory().If(condition, body, null);
+    }
+
+    static JCReturn retuurn(JCExpression value) {
+        return factory().Return(value);
+    }
+
+    static JCLiteral truu() {
+        return factory().Literal(TRUE);
+    }
+
+    static JCExpression thiz() {
+        return factory().This(objectType());
+    }
+
+    static JCIdent identifier(String name) {
+        return factory().Ident(names().fromString(name));
+    }
+
+    static JCLiteral literal(Object literal) {
+        return factory().Literal(literal);
+    }
+
+    static JCBinary isEqual(JCExpression left, JCExpression right) {
+        return factory().Binary(EQ, left, right);
+    }
+
+    static Type objectType() {
+        return symtab().objectType;
+    }
+
+    static Type intType() {
+        return symtab().intType;
+    }
+
+    private static TreeMaker factory() {
+        return TreeMaker.instance(context());
+    }
+
+    private static Names names() {
+        return Names.instance(context());
+    }
+
+    private static Symtab symtab() {
+        return Symtab.instance(context());
+    }
+
+    private static Context context() {
+        return CONTEXT_THREAD_LOCAL.get();
+    }
+
+}
--- cebolla-compiler-plugin/src/main/java/com/github/cstettler/cebolla/plugin/CebollaStereotypesPlugin.java
+++ cebolla-compiler-plugin/src/main/java/com/github/cstettler/cebolla/plugin/CebollaStereotypesPlugin.java
@@ -8,60 +8,55 @@ import com.sun.source.util.TaskEvent;
 import com.sun.source.util.TaskListener;
 import com.sun.source.util.TreeScanner;
 import com.sun.tools.javac.api.BasicJavacTask;
-import com.sun.tools.javac.code.Flags;
-import com.sun.tools.javac.code.Symtab;
-import com.sun.tools.javac.code.Type;
-import com.sun.tools.javac.code.TypeTag;
-import com.sun.tools.javac.tree.JCTree.JCArrayTypeTree;
-import com.sun.tools.javac.tree.JCTree.JCBinary;
 import com.sun.tools.javac.tree.JCTree.JCBlock;
 import com.sun.tools.javac.tree.JCTree.JCClassDecl;
-import com.sun.tools.javac.tree.JCTree.JCExpression;
-import com.sun.tools.javac.tree.JCTree.JCFieldAccess;
-import com.sun.tools.javac.tree.JCTree.JCIdent;
-import com.sun.tools.javac.tree.JCTree.JCIf;
-import com.sun.tools.javac.tree.JCTree.JCLiteral;
-import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
-import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
-import com.sun.tools.javac.tree.JCTree.JCPrimitiveTypeTree;
-import com.sun.tools.javac.tree.JCTree.JCReturn;
-import com.sun.tools.javac.tree.JCTree.JCStatement;
-import com.sun.tools.javac.tree.JCTree.JCUnary;
-import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
-import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.Name;
-import com.sun.tools.javac.util.Names;
 
 import java.lang.annotation.Annotation;
-import java.util.stream.Stream;
 
+import static com.github.cstettler.cebolla.plugin.AstUtils.add;
+import static com.github.cstettler.cebolla.plugin.AstUtils.addMethod;
+import static com.github.cstettler.cebolla.plugin.AstUtils.arguments;
+import static com.github.cstettler.cebolla.plugin.AstUtils.block;
+import static com.github.cstettler.cebolla.plugin.AstUtils.callTo;
+import static com.github.cstettler.cebolla.plugin.AstUtils.cast;
+import static com.github.cstettler.cebolla.plugin.AstUtils.falze;
+import static com.github.cstettler.cebolla.plugin.AstUtils.fieldOrMethod;
+import static com.github.cstettler.cebolla.plugin.AstUtils.fieldsOf;
+import static com.github.cstettler.cebolla.plugin.AstUtils.identifier;
+import static com.github.cstettler.cebolla.plugin.AstUtils.iif;
+import static com.github.cstettler.cebolla.plugin.AstUtils.intType;
+import static com.github.cstettler.cebolla.plugin.AstUtils.isArray;
+import static com.github.cstettler.cebolla.plugin.AstUtils.isEqual;
+import static com.github.cstettler.cebolla.plugin.AstUtils.isNotEqual;
+import static com.github.cstettler.cebolla.plugin.AstUtils.isPrimitive;
+import static com.github.cstettler.cebolla.plugin.AstUtils.isPrimitiveOfType;
+import static com.github.cstettler.cebolla.plugin.AstUtils.literal;
+import static com.github.cstettler.cebolla.plugin.AstUtils.method;
+import static com.github.cstettler.cebolla.plugin.AstUtils.multiply;
+import static com.github.cstettler.cebolla.plugin.AstUtils.not;
+import static com.github.cstettler.cebolla.plugin.AstUtils.nuull;
+import static com.github.cstettler.cebolla.plugin.AstUtils.objectType;
+import static com.github.cstettler.cebolla.plugin.AstUtils.or;
+import static com.github.cstettler.cebolla.plugin.AstUtils.parameter;
+import static com.github.cstettler.cebolla.plugin.AstUtils.reAssignVariable;
+import static com.github.cstettler.cebolla.plugin.AstUtils.retuurn;
+import static com.github.cstettler.cebolla.plugin.AstUtils.thiz;
+import static com.github.cstettler.cebolla.plugin.AstUtils.truu;
+import static com.github.cstettler.cebolla.plugin.AstUtils.variable;
+import static com.github.cstettler.cebolla.plugin.AstUtils.with;
 import static com.sun.source.util.TaskEvent.Kind.PARSE;
-import static com.sun.tools.javac.code.Flags.FINAL;
 import static com.sun.tools.javac.code.Flags.PUBLIC;
 import static com.sun.tools.javac.code.TypeTag.BOOLEAN;
-import static com.sun.tools.javac.code.TypeTag.BOT;
 import static com.sun.tools.javac.code.TypeTag.DOUBLE;
 import static com.sun.tools.javac.code.TypeTag.FLOAT;
 import static com.sun.tools.javac.code.TypeTag.INT;
-import static com.sun.tools.javac.tree.JCTree.Tag.EQ;
-import static com.sun.tools.javac.tree.JCTree.Tag.MUL;
-import static com.sun.tools.javac.tree.JCTree.Tag.NE;
-import static com.sun.tools.javac.tree.JCTree.Tag.NOT;
-import static com.sun.tools.javac.tree.JCTree.Tag.OR;
-import static com.sun.tools.javac.tree.JCTree.Tag.PLUS;
 import static com.sun.tools.javac.util.List.from;
 import static com.sun.tools.javac.util.List.nil;
 import static com.sun.tools.javac.util.List.of;
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
-import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 public class CebollaStereotypesPlugin implements Plugin {
-
-    private static final ThreadLocal<Context> CONTEXT_THREAD_LOCAL = new ThreadLocal<>();
 
     @Override
     public String getName() {
@@ -235,211 +230,6 @@ public class CebollaStereotypesPlugin implements Plugin {
                 }, null);
             }
         });
-    }
-
-    private static void with(Context context, Runnable runnable) {
-        try {
-            CONTEXT_THREAD_LOCAL.set(context);
-
-            runnable.run();
-        } finally {
-            CONTEXT_THREAD_LOCAL.remove();
-        }
-    }
-
-    private static void addMethod(JCClassDecl classDecl, JCMethodDecl equalsMethodDecl) {
-        classDecl.defs = classDecl.defs.append(equalsMethodDecl);
-    }
-
-    private static List<JCExpression> arguments(Stream<JCExpression> stream) {
-        return from(stream.collect(toList()));
-    }
-
-    private static List<JCExpression> arguments(JCExpression... expressions) {
-        return from(asList(expressions));
-    }
-
-    private static JCMethodDecl method(int modifier, TypeTag returnType, String name, List<JCVariableDecl> parameters, JCBlock equalsBody) {
-        return TreeMaker.instance(context()).MethodDef(
-                TreeMaker.instance(context()).Modifiers(modifier),
-                Names.instance(context()).fromString(name),
-                TreeMaker.instance(context()).TypeIdent(returnType),
-                nil(),
-                parameters,
-                nil(),
-                equalsBody,
-                null
-        );
-    }
-
-    private static Stream<JCVariableDecl> fieldsOf(JCClassDecl classDeclaration) {
-        return classDeclaration.getMembers().stream()
-                .filter((member) -> member instanceof JCVariableDecl)
-                .map((member) -> (JCVariableDecl) member);
-    }
-
-    private static JCVariableDecl parameter(JCClassDecl classDeclaration, String parameterName, Type parameterType) {
-        return TreeMaker.instance(context()).at(classDeclaration.pos).Param(Names.instance(context()).fromString(parameterName), parameterType, null);
-    }
-
-    private static JCBlock block(List<JCStatement> statements) {
-        return TreeMaker.instance(context()).Block(0, statements);
-    }
-
-    private static JCUnary not(JCExpression expression) {
-        return TreeMaker.instance(context()).Unary(NOT, expression);
-    }
-
-    private static boolean isArray(JCVariableDecl fieldDeclaration) {
-        return fieldDeclaration.vartype instanceof JCArrayTypeTree;
-    }
-
-    private static JCExpression identifier(Name name) {
-        return TreeMaker.instance(context()).Ident(name);
-    }
-
-    private static boolean isPrimitive(JCVariableDecl fieldDeclaration) {
-        return fieldDeclaration.vartype instanceof JCPrimitiveTypeTree;
-    }
-
-    private static boolean isPrimitiveOfType(JCVariableDecl fieldDeclaration, TypeTag typeTag) {
-        return isPrimitive(fieldDeclaration)
-                && fieldDeclaration.vartype instanceof JCPrimitiveTypeTree
-                && ((JCPrimitiveTypeTree) fieldDeclaration.vartype).typetag == typeTag;
-    }
-
-    private static JCLiteral falze() {
-        return TreeMaker.instance(context()).Literal(FALSE);
-    }
-
-    private static JCLiteral nuull() {
-        return TreeMaker.instance(context()).Literal(BOT, null);
-    }
-
-    private static JCBinary or(JCExpression left, JCExpression right) {
-        return TreeMaker.instance(context()).Binary(OR, left, right);
-    }
-
-    private static JCBinary isNotEqual(JCExpression left, JCExpression right) {
-        return TreeMaker.instance(context()).Binary(NE, left, right);
-    }
-
-    private static JCBinary add(JCExpression left, JCExpression right) {
-        return TreeMaker.instance(context()).Binary(PLUS, left, right);
-    }
-
-    private static JCBinary multiply(JCExpression left, JCExpression right) {
-        return TreeMaker.instance(context()).Binary(MUL, left, right);
-    }
-
-    private static JCStatement reAssignVariable(JCIdent variable, JCBinary expression) {
-        return TreeMaker.instance(context()).Exec(TreeMaker.instance(context()).Assign(variable, expression));
-    }
-
-    private static JCMethodInvocation callTo(JCExpression method, JCExpression... arguments) {
-        return callTo(method, from(arguments));
-    }
-
-    private static JCMethodInvocation callTo(JCExpression method, List<JCExpression> arguments) {
-        return TreeMaker.instance(context()).Apply(nil(), method, arguments);
-    }
-
-    private static JCFieldAccess fieldOrMethod(String fieldOrMethod) {
-        return TreeMaker.instance(context()).Select(thiz(), Names.instance(context()).fromString(fieldOrMethod));
-    }
-
-    private static JCFieldAccess fieldOrMethod(String target, String fieldOrMethod) {
-        return fieldOrMethod(target, Names.instance(context()).fromString(fieldOrMethod));
-    }
-
-    private static JCFieldAccess fieldOrMethod(String target, Name fieldOrMethod) {
-        if (target.contains(".")) {
-            String[] classNameParts = target.split("\\.");
-            JCExpression classIdentifier = identifier(classNameParts[0]);
-
-            for (int i = 1; i < classNameParts.length; i++) {
-                classIdentifier = TreeMaker.instance(context()).Select(classIdentifier, Names.instance(context()).fromString(classNameParts[i]));
-            }
-
-            return TreeMaker.instance(context()).Select(
-                    classIdentifier,
-                    fieldOrMethod
-            );
-        } else {
-            return TreeMaker.instance(context()).Select(identifier(target), fieldOrMethod);
-        }
-    }
-
-    private static JCVariableDecl cast(boolean makeFinal, String targetVariableName, String sourceVariableName, JCClassDecl targetType) {
-        return variable(
-                makeFinal,
-                identifier(targetType.name),
-                targetVariableName,
-                TreeMaker.instance(context()).TypeCast(identifier(targetType.name), identifier(sourceVariableName))
-        );
-    }
-
-    private static JCVariableDecl variable(boolean makeFinal, Type type, String name, JCExpression initialization) {
-        return variable(makeFinal, TreeMaker.instance(context()).Type(type), name, initialization);
-    }
-
-    private static JCVariableDecl variable(boolean makeFinal, JCExpression type, String name, JCExpression initialization) {
-        return TreeMaker.instance(context()).VarDef(
-                TreeMaker.instance(context()).Modifiers(makeFinal ? FINAL : 0),
-                Names.instance(context()).fromString(name),
-                type,
-                initialization
-        );
-    }
-
-    private static JCIf iif(JCExpression condition, JCStatement body) {
-        return TreeMaker.instance(context()).If(
-                condition,
-                body,
-                null
-        );
-    }
-
-    private static JCReturn retuurn(JCExpression value) {
-        return TreeMaker.instance(context()).Return(value);
-    }
-
-    private static JCLiteral truu() {
-        return TreeMaker.instance(context()).Literal(TRUE);
-    }
-
-    private static JCExpression thiz() {
-        return TreeMaker.instance(context()).This(objectType());
-    }
-
-    private static JCIdent identifier(String name) {
-        TreeMaker factory = TreeMaker.instance(context());
-        Names names = Names.instance(context());
-
-        return factory.Ident(names.fromString(name));
-    }
-
-
-    private static JCLiteral literal(Object literal) {
-        return TreeMaker.instance(context()).Literal(literal);
-    }
-
-    private static JCBinary isEqual(JCExpression left, JCExpression right) {
-        TreeMaker factory = TreeMaker.instance(context());
-
-        return factory.Binary(EQ, left, right);
-    }
-
-    private static Type objectType() {
-        return Symtab.instance(context()).objectType;
-    }
-
-    private static Type intType() {
-        return Symtab.instance(context()).intType;
-    }
-
-    private static Context context() {
-        return CONTEXT_THREAD_LOCAL.get();
     }
 
 }
