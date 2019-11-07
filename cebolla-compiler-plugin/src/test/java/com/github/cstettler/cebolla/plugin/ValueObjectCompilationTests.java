8463d13046cb9c52ecc4d4abe3e56e0dc457ee14
cstettler <cstettler@imap.ch>
2019-11-07 16:01:59 +0100

    extracted compilation-only tests for value objects (correct behaviour of value objects will be tested with plugin integration tests)

---
+++ cebolla-compiler-plugin/src/test/java/com/github/cstettler/cebolla/plugin/ValueObjectCompilationTests.java
@@ -0,0 +1,130 @@
+package com.github.cstettler.cebolla.plugin;
+
+import com.google.testing.compile.Compilation;
+import org.junit.jupiter.api.Test;
+
+import javax.tools.JavaFileObject;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+import static com.google.testing.compile.Compiler.javac;
+import static com.google.testing.compile.JavaFileObjects.forSourceLines;
+
+class ValueObjectCompilationTests {
+
+    @Test
+    void compile_valueObjectWithStringState_succeeds() {
+        // arrange
+        JavaFileObject sourceFile = forSourceLines("com.github.cstettler.cebolla.test.TestValueObject",
+                "package com.github.cstettler.cebolla.test;",
+                "",
+                "import com.github.cstettler.cebolla.stereotype.ValueObject;",
+                "",
+                "@ValueObject",
+                "public class TestValueObject {",
+                "   private final String valueOne;",
+                "   private final String valueTwo;",
+                "",
+                "  public TestValueObject(String valueOne, String valueTwo) {",
+                "    this.valueOne = valueOne;",
+                "    this.valueTwo = valueTwo;",
+                "  }",
+                "}"
+        );
+
+        // act
+        Compilation compilation = compile(sourceFile);
+
+        // assert
+        assertThat(compilation).succeededWithoutWarnings();
+    }
+
+    @Test
+    void compile_valueObjectWithPrimitiveState_succeeds() {
+        // arrange
+        JavaFileObject sourceFile = forSourceLines("com.github.cstettler.cebolla.test.TestValueObject",
+                "package com.github.cstettler.cebolla.test;",
+                "",
+                "import com.github.cstettler.cebolla.stereotype.ValueObject;",
+                "",
+                "@ValueObject",
+                "public class TestValueObject {",
+                "   private final int valueOne;",
+                "   private final boolean valueTwo;",
+                "   private final double valueThree;",
+                "",
+                "  public TestValueObject(int valueOne, boolean valueTwo, double valueThree) {",
+                "    this.valueOne = valueOne;",
+                "    this.valueTwo = valueTwo;",
+                "    this.valueThree = valueThree;",
+                "  }",
+                "}"
+        );
+
+        // act
+        Compilation compilation = compile(sourceFile);
+
+        // assert
+        assertThat(compilation).succeededWithoutWarnings();
+    }
+
+    @Test
+    void compile_valueObjectWithArrayState_succeeds() {
+        // arrange
+        JavaFileObject sourceFile = forSourceLines("com.github.cstettler.cebolla.test.TestValueObject",
+                "package com.github.cstettler.cebolla.test;",
+                "",
+                "import com.github.cstettler.cebolla.stereotype.ValueObject;",
+                "",
+                "@ValueObject",
+                "public class TestValueObject {",
+                "   private final String[] valueOne;",
+                "   private final String[] valueTwo;",
+                "",
+                "  public TestValueObject(String[] valueOne, String[] valueTwo) {",
+                "    this.valueOne = valueOne;",
+                "    this.valueTwo = valueTwo;",
+                "  }",
+                "}"
+        );
+
+        // act
+        Compilation compilation = compile(sourceFile);
+
+        // assert
+        assertThat(compilation).succeededWithoutWarnings();
+    }
+
+    @Test
+    void compile_valueObjectWithArrayAndObjectState_succeeds() {
+        // arrange
+        JavaFileObject sourceFile = forSourceLines("com.github.cstettler.cebolla.test.TestValueObject",
+                "package com.github.cstettler.cebolla.test;",
+                "",
+                "import com.github.cstettler.cebolla.stereotype.ValueObject;",
+                "",
+                "@ValueObject",
+                "public class TestValueObject {",
+                "   private final String[] valueOne;",
+                "   private final String valueTwo;",
+                "",
+                "  public TestValueObject(String[] valueOne, String valueTwo) {",
+                "    this.valueOne = valueOne;",
+                "    this.valueTwo = valueTwo;",
+                "  }",
+                "}"
+        );
+
+        // act
+        Compilation compilation = compile(sourceFile);
+
+        // assert
+        assertThat(compilation).succeededWithoutWarnings();
+    }
+
+    private static Compilation compile(JavaFileObject sourceFile) {
+        return javac()
+                .withOptions("-Xplugin:CebollaStereotype")
+                .compile(sourceFile);
+    }
+
+}
